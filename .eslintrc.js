948ae2d6759d3140f56c8b7484c6ee02aefe89b5
Bragolgirith <vasil.toshev@gmail.com>
2020-07-12 17:12:14 +0200

    Allow console

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'vuetify'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/html-closing-bracket-newline': ['error', {
       singleline: 'never',
